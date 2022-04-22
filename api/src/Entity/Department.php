ce5868a551315f1c0eb9f0cc7554411ce26819ec
jackern <jackahern15@gmail.com>
2022-04-22 16:29:50 +0100

    add JobRole and Department entities, remove dummy entity from API-Platform

--- api/src/Entity/Department.php
+++ api/src/Entity/Department.php
@@ -3,19 +3,25 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\DepartmentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
-#[ORM\Entity]
+#[ORM\Entity(repositoryClass: DepartmentRepository::class)]
 #[ApiResource]
 class Department
 {
-    private ?int $id = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
 
-    public string $title = '';
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
 
-    /** @var JobRole[] */
-    public ?iterable $jobRoles = null;
+    #[ORM\ManyToMany(targetEntity: JobRole::class, mappedBy: 'department')]
+    private $jobRoles;
 
     public function __construct()
     {
@@ -25,5 +31,44 @@ class Department
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|JobRole[]
+     */
+    public function getJobRoles(): Collection
+    {
+        return $this->jobRoles;
+    }
+
+    public function addJobRole(JobRole $jobRole): self
+    {
+        if (!$this->jobRoles->contains($jobRole)) {
+            $this->jobRoles[] = $jobRole;
+            $jobRole->addDepartment($this);
+        }
+
+        return $this;
+    }
+
+    public function removeJobRole(JobRole $jobRole): self
+    {
+        if ($this->jobRoles->removeElement($jobRole)) {
+            $jobRole->removeDepartment($this);
+        }
+
+        return $this;
     }
 }
--- api/src/Entity/JobRole.php
+++ api/src/Entity/JobRole.php
@@ -3,14 +3,25 @@
 namespace App\Entity;
 
 use ApiPlatform\Core\Annotation\ApiResource;
+use App\Repository\JobRoleRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
 
+#[ORM\Entity(repositoryClass: JobRoleRepository::class)]
 #[ApiResource]
 class JobRole
 {
-    private ?int $id = null;
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
 
-    /** @var Department[] */
-    public ?iterable $department = null;
+    #[ORM\Column(type: 'string', length: 255)]
+    private $title;
+
+    #[ORM\ManyToMany(targetEntity: Department::class, inversedBy: 'jobRoles')]
+    private $department;
 
     public function __construct()
     {
@@ -20,5 +31,41 @@ class JobRole
     public function getId(): ?int
     {
         return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Department[]
+     */
+    public function getDepartment(): Collection
+    {
+        return $this->department;
+    }
+
+    public function addDepartment(Department $department): self
+    {
+        if (!$this->department->contains($department)) {
+            $this->department[] = $department;
+        }
+
+        return $this;
+    }
+
+    public function removeDepartment(Department $department): self
+    {
+        $this->department->removeElement($department);
+
+        return $this;
     }
 }
